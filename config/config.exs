646534d9b7e3a691ef7bdb8a5cf29ca09d186181
Justin Toniazzo <jutonz42@gmail.com>
2020-03-29 15:27:47 -0400

    only require api key in prod

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 import Config
 
-config :flow, api_key: System.fetch_env!("FLOW_API_KEY")
+config :flow, api_key: System.get_env("FLOW_API_KEY")
 
 import_config "#{Mix.env()}.exs"
--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :flow,
+  api_key: System.fetch_env!("FLOW_API_KEY"),
   monitors: [
     %{pin: 24, log_id: "f150cb6e-f0e0-4674-a5cc-a22b3fa3df28"}
   ]
