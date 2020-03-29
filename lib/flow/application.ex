318a3fa5712481e7da71180f29aab7286bea7ad7
Justin Toniazzo <jutonz42@gmail.com>
2020-03-29 09:55:28 -0400

    Initial commit

---
+++ lib/flow/application.ex
@@ -0,0 +1,19 @@
+defmodule Flow.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Flow.Worker.start_link(arg)
+      # {Flow.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Flow.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
