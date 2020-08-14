f2a20ab046ac1492e0bd3e90ede24148960ac1fa
Justin Toniazzo <jutonz42@gmail.com>
2020-08-13 21:02:40 -0400

    correct typo in sleep

--- lib/flow/screen/linux_provider.ex
+++ lib/flow/screen/linux_provider.ex
@@ -1,6 +1,6 @@
 defmodule Flow.Screen.LinuxProvider do
   def sleep do
-    System.cmd("xset", ~w[dpms force on], env: [{"DISPLAY", ":0"}])
+    System.cmd("xset", ~w[dpms force off], env: [{"DISPLAY", ":0"}])
   end
 
   def wake do
