dcef84559a32e78be2cea6a8fd3a962ac5e654b5
Justin Toniazzo <jutonz@hey.com>
2020-10-02 09:42:01 -0400

    Add scale support
    Use a python script and library to interact with an HX711 load cell and measure weight.

--- lib/flow/scale.ex
+++ lib/flow/scale.ex
@@ -13,15 +13,46 @@ defmodule Flow.Scale do
 
   def handle_continue(nil, state) do
     {:ok, py_pid} = :python.start([
-      {:python_path, ["/home/pi/code/hx711py"]}
+      python_path: to_char_list("/home/pi/code/jutonz/flow-ex/vendor")
     ])
-    send(self(), :check)
+    setup_scale(py_pid)
+    send(self(), :get_measurement)
     {:noreply, Map.put(state, :py_pid, py_pid)}
   end
 
-  def handle_info(:check, %{py_pid: pid} = state) do
-    version = :python.call(pid, :sys, :"version.__str__", [])
-    Logger.info(version)
+  def handle_info(:get_measurement, %{py_pid: pid} = state) do
+    value = get_measurement(pid)
+    value = max(0, value)
+    log("Got measurement: #{value}")
+    schedule_measurement()
     {:noreply, state}
+  end
+
+  def terminate(_reason, %{py_pid: pid} = _state) do
+    log("Received terminate. Cleaning up...")
+    cleanup(pid)
+    :normal
+  end
+
+  defp schedule_measurement do
+    Process.send_after(self(), :get_measurement, 1000)
+  end
+
+  defp get_measurement(pid) do
+    :python.call(pid, :scale, :get_measurement, [])
+  end
+
+  defp setup_scale(pid) do
+    log("Initializing scale...")
+    :python.call(pid, :scale, :setup, [])
+    log("Scale initialized")
+  end
+
+  defp cleanup(pid) do
+    :python.call(pid, :scale, :teardown, [])
+  end
+
+  defp log(message) do
+    Logger.info("[#{__MODULE__}] #{message}")
   end
 end
---
+++ vendor/scale.py
@@ -0,0 +1,48 @@
+import time
+import sys
+import RPi.GPIO as GPIO
+from hx711 import HX711
+from erlport.erlterms import Atom
+from erlport.erlang import cast
+
+class Scale:
+    def __init__(self):
+        self.scale = HX711(5, 6)
+        self.scale.set_reading_format("MSB", "MSB")
+        self.scale.set_reference_unit(428)
+        self.scale.reset()
+        self.scale.tare()
+
+    def get_measurement(self):
+        weight = self.scale.get_weight(5)
+        self.scale.power_down()
+        self.scale.power_up()
+        return weight
+
+class Scale2:
+    def get_measurement(self):
+        return 123
+
+scale = None
+
+def setup():
+    global scale
+    scale = Scale()
+
+def teardown():
+    GPIO.cleanup()
+
+def get_measurement():
+    return scale.get_measurement()
+
+
+# def start(dest):
+    # while True:
+        # time.sleep(0.5)
+        # cast(dest, (Atom("measurement"), 123))
+    # scale = HX711(5, 6)
+    # scale.set_reading_format("MSB", "MSB")
+    # scale.set_reference_unit(428)
+    # scale.reset()
+    # scale.tare()
+
