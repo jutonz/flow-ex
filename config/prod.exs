5f2ffe63adcf112a0bb0d134e77b3416a70d722c
Justin Toniazzo <jutonz42@gmail.com>
2020-12-13 00:03:04 -0500

    Say scale is not disabled in prod

--- config/prod.exs
+++ config/prod.exs
@@ -5,6 +5,9 @@ config :flow,
   monitors: [
     %{pin: 24, log_id: "f150cb6e-f0e0-4674-a5cc-a22b3fa3df28"}
   ],
+  scale: [
+    disabled: false
+  ],
   screen_provider: Flow.Screen.LinuxProvider,
   socket_url: "wss://app.jutonz.com/socket/websocket",
   humidity: [
